5eef675e5a6e8e482199a73a654a427709488b86
darshana8 <75625956+darshana8@users.noreply.github.com>
2023-09-01 12:54:48 +0530

    day 1

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# Frontend
+1.created component header 
+2.list of foods
+    model
+    data.ts
+    add sample foods
+    food services
+    home component
+
+    component ===>shared==>model==>Food.ts
+       
+
+
+<!-- # Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.0.
 
@@ -24,4 +36,4 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 
 ## Further help
 
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page. -->
---
+++ src/data.ts
@@ -0,0 +1,139 @@
+import { Food } from "./app/components/partial/header/shared/model/Food";
+
+
+export const sample_foods:Food[]=[
+    {
+        id:'1',
+        name:'Pizza',
+        price:500,
+        tags:['fastfood','Lunch','brunch','Dinner'],
+        favorite:false,
+        origins:['Italy'],
+        stars:3.0,
+        imageUrl:'assets/food-1.jpg',
+        cookTime:'40-50'
+    },
+
+    {
+        id:'2',
+        name:'Meat balls',
+        price:400,
+        tags:['starter','Lunch','brunch','Dinner'],
+        favorite:false,
+        origins:['India'],
+        stars:4.0,
+        imageUrl:'assets/food-2.jpg',
+        cookTime:'30-40'
+    },
+    {
+        id:'3',
+        name:'Burger',
+        price:300,
+        tags:['fastfood','brunch'],
+        favorite:false,
+        origins:['Italy','America'],
+        stars:3.5,
+        imageUrl:'assets/food-3.jpg',
+        cookTime:'40-50'
+    },
+    {
+        id:'4',
+        name:'French fries',
+        price:150,
+        tags:['fastfood','brunch'],
+        favorite:false,
+        origins:['Italy','Japan'],
+        stars:3.0,
+        imageUrl:'assets/food-4.jpg',
+        cookTime:'40-50'
+    },
+    {
+        id:'5',
+        name:'Chiken - Soup',
+        price:250,
+        tags:['starter'],
+        favorite:false,
+        origins:['India'],
+        stars:3.0,
+        imageUrl:'assets/food-5.jpg',
+        cookTime:'40-50'
+    },
+    {
+        id:'6',
+        name:'Pizza',
+        price:500,
+        tags:['fastfood','Lunch','brunch','Dinner'],
+        favorite:false,
+        origins:['India'],
+        stars:4.0,
+        imageUrl:'assets/food-6.jpg',
+        cookTime:'40-50'
+    },
+    {
+        id:'7',
+        name:'Chicken Biryani',
+        price:500,
+        tags:['fastfood','Lunch','brunch','Dinner'],
+        favorite:false,
+        origins:['Italy'],
+        stars:3.0,
+        imageUrl:'assets/food-7.jpeg',
+        cookTime:'40-50'
+    },
+    {
+        id:'8',
+        name:'Momos',
+        price:500,
+        tags:['fastfood','brunch'],
+        favorite:false,
+        origins:['Italy'],
+        stars:4.0,
+        imageUrl:'assets/food-8.jpeg',
+        cookTime:'40-50'
+    },
+
+    {
+        id:'9',
+        name:'Chicken Nuggets',
+        price:600,
+        tags:['fastfood','Lunch','brunch','Dinner'],
+        favorite:false,
+        origins:['India'],
+        stars:4.0,
+        imageUrl:'assets/food-9.jpeg',
+        cookTime:'10-20'
+    },
+    {
+        id:'10',
+        name:'Hakka Noodles',
+        price:200,
+        tags:['fastfood','Lunch','brunch','Dinner'],
+        favorite:false,
+        origins:['Italy','China'],
+        stars:5.0,
+        imageUrl:'assets/food-10.jpeg',
+        cookTime:'20-30'
+    },
+    {
+        id:'11',
+        name:'Full Meal',
+        price:750,
+        tags:['Lunch','brunch','Dinner'],
+        favorite:false,
+        origins:['India'],
+        stars:4.0,
+        imageUrl:'assets/food-11.jpeg',
+        cookTime:'40-50'
+    },
+    {
+        id:'12',
+        name:'Paratha',
+        price:300,
+        tags:['Lunch','Dinner'],
+        favorite:false,
+        origins:['India'],
+        stars:3.0,
+        imageUrl:'assets/food-12.jpeg',
+        cookTime:'40-50'
+    }
+]
